c1bbd1a332ce8b3355b6a77d3f37caf99849e7c1
JBJHARDWARE03 <JBJhardware03@gmail.com>
2021-06-02 22:41:03 -0700

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import './Home.css'
-
+import logo from "./Logo.png";
+import gear from "./gear.gif";
 export class Home extends Component{
 render(){
 return(
 <section className="section1">
-<img className="logo" src="Logo.png" width="270px"></img>
-<img className="img" src="gear.gif" ></img>
+<img className="logo" src={logo} width="270px"></img>
+<img className="img" src={gear} ></img>
     <div className="container1">
         <ul className="navigation">
             <li><a href="#">Home</a></li>
